e533dcab7e073b38270a6fb59486a5fa630b0020
AlexBirdSong <alexwhitechoco@gmail.com>
2020-09-21 22:34:01 +0300

    add exercise solution 'Count descendants'

--- script1.js
+++ script1.js
@@ -2,7 +2,7 @@
 
 for (let li of document.querySelectorAll('li')) {
     let title = li.firstChild.data;
-    let lenghtLi = li.getElementsByTagName('li').length
-    alert(lenghtLi);
+    let lenghtLi = li.getElementsByTagName('li').length;
+    alert(title + ": " + lenghtLi);
   }
 
