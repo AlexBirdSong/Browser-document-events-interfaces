0b7b9f33fe45b8b6b59280ddf5aa519dc05bb30c
AlexBirdSong <alexwhitechoco@gmail.com>
2020-09-22 22:58:07 +0300

    Add execise solution 'orange link'

--- script3.js
+++ script3.js
@@ -2,9 +2,12 @@
 
 let elements  = document.querySelectorAll('li > a');
 for (let a of elements) { 
-    alert( a.innerHTML );
+    // alert( a.innerHTML );
+    // alert(a.getAttribute('href'));
+    let aLink = a.getAttribute('href');
+    if(!aLink.startsWith('http://internal.com') && aLink.indexOf("://") != -1){
+        a.style.color = 'orange';
+    }
   }
 
 
-let link = document.querySelector('a');
-link.style.color = 'orange';
