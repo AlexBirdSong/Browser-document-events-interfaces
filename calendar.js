37b79450222af72c34f8827a1724d08cc0c8e87b
AlexBirdSong <alexwhitechoco@gmail.com>
2020-09-26 16:59:10 +0300

    add function dayInMonth

--- calendar.js
+++ calendar.js
@@ -11,13 +11,21 @@ function getLocalDay(date) {
     return day;
   }
 
+  function dayInMonth(date){
+    let currdate = date;
+    let nextdate = new Date(currdate.getFullYear(), currdate.getMonth()+1, currdate.getDate());
+    let countDays = Math.round((nextdate - currdate) / 1000 / 3600 / 24);
+
+    return countDays;
+  }
+
 function createCalendar(elem, year, month){
     let date = new Date(year, month, 3); 
     let day = getLocalDay(date);
-    
+
     elem.innerHTML="<tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr>"; 
 }
 
 let elem = document.getElementById('cal');
 
-createCalendar(elem, 2019, 5);
+ createCalendar(elem, 2020, 9);
