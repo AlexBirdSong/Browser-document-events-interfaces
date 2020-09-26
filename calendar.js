e3f0cbcefe579b54e874bc9bb022e01b3acfaec2
AlexBirdSong <alexwhitechoco@gmail.com>
2020-09-26 16:41:26 +0300

    crate header calendar

--- calendar.js
+++ calendar.js
@@ -1,1 +1,10 @@
 'use strict'
+
+function createCalendar(elem, year, month){
+
+    elem.innerHTML="<tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr>"; 
+}
+
+let elem = document.getElementById('cal');
+
+createCalendar(elem, 2019, 5);
