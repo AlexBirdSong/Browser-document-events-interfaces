b43248f2c298db6eab7afd197d25706bc581ebc9
AlexBirdSong <alexwhitechoco@gmail.com>
2020-09-28 21:57:25 +0300

    add exercise soltion 'clock'

--- clock.js
+++ clock.js
@@ -1,2 +1,19 @@
 'use strict'
 
+function drawClock(){
+  let currDate = new Date();
+  let clockString = "<tr>";
+  
+  let hours = currDate.getHours();
+  let minutes = currDate.getMinutes();
+  let seconds = currDate.getSeconds();
+  
+  clockString +="<td id='hr'>" + hours + "</td><td id='mn'>" + minutes + "</td><td id='sc'>" + seconds + "</td>";
+  clockString += "</tr>";
+
+  return clockString;
+}
+
+let elem = document.getElementById('clock');
+
+setInterval(() => elem.innerHTML=drawClock(),1000);
