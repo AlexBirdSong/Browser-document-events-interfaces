d2367725690cf007aab8d2522a94f917b5d5307d
AlexBirdSong <alexwhitechoco@gmail.com>
2020-10-06 21:18:24 +0300

    add exercise solution 'ball'

--- ball.js
+++ ball.js
@@ -1,1 +1,16 @@
 'use strict'
+
+let ball = document.getElementById('ball');
+let field = document.getElementById('field');
+let ballHeight,ballWidth;
+
+ball.onload = function() {      
+    ballHeight = this.width;
+    ballWidth = this.height;
+
+    let ballTopPosition = Math.floor((field.clientHeight-ballHeight)/2);
+    let ballLeftPosition = Math.floor((field.clientWidth-ballWidth)/2);
+    ball.style.marginTop = ballTopPosition+"px";
+    ball.style.marginLeft = ballLeftPosition+"px";
+
+  }   
