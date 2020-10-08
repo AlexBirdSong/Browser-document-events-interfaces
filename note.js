78743b605af98b4cbb03152f0319f507199f6007
AlexBirdSong <alexwhitechoco@gmail.com>
2020-10-08 22:18:36 +0300

    create application 'note'

---
+++ note.js
@@ -0,0 +1,33 @@
+'use strict'
+ /**
+     * Позиционирует элемент elem относительно элемента anchor в соответствии со значением position.
+     *
+     * @param {Node} anchor     элемент, около которого позиционируется другой элемент
+     * @param {string} position одно из: top/right/bottom
+     * @param {Node} elem       элемент, который позиционируется
+     *
+     * Оба элемента elem и anchor должны присутствовать в документе
+     */
+    function positionAt(anchor, position, elem) {
+        // ... ваш код ...
+      }
+  
+      /**
+       * Показывает заметку с заданным содержимым на заданной позиции
+       * относительно элемента anchor.
+       */
+      function showNote(anchor, position, html) {
+        let note = document.createElement('div');
+        note.className = "note";
+        note.innerHTML = html;
+        document.body.append(note);
+  
+        positionAt(anchor, position, note);
+      }
+  
+      // test it
+      let blockquote = document.querySelector('blockquote');
+  
+      showNote(blockquote, "top", "note above");
+      showNote(blockquote, "right", "note at the right");
+      showNote(blockquote, "bottom", "note below");
