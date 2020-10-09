9198343989418dd1980e3dc9fb94aa10f9b1ecb6
AlexBirdSong <alexwhitechoco@gmail.com>
2020-10-09 23:36:54 +0300

    add exercise solution 'find cord'

--- findCord.js
+++ findCord.js
@@ -1,2 +1,8 @@
 'use strict'
 
+let elem = document.getElementById('field');
+let coord = elem.getBoundingClientRect();
+alert("Координаты первого: x: " + coord.left + " y: " + coord.top );
+lert("Координаты второго: x: " + coord.right + " y: " + coord.bottom );
+alert("Координаты третьего: x: " + (coord.left + elem.clientLeft) + " y: " + (coord.top + elem.clientTop));
+alert("Координаты четвертого: x: " + (coord.right - elem.clientLeft) + " y: " + (coord.bottom - elem.clientTop));
