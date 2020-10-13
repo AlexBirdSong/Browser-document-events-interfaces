a56bcddc1374bc00ae7775259b86a79c96c455c3
AlexBirdSong <alexwhitechoco@gmail.com>
2020-10-13 23:32:29 +0300

    modified: now the ball does not fly out of the field

--- ball.js
+++ ball.js
@@ -8,15 +8,54 @@ ball.onload = function() {
     ballHeight = this.width;
     ballWidth = this.height;
 
+    let bottomBorder = field.clientHeight;
+    let rightBorder = field.clientWidth;
+
     field.addEventListener('click', function(event) {
       let xPosition = event.clientX;
       let yPosition = event.clientY; 
 
-      let ballTopPosition = Math.floor((yPosition  - ballHeight));
-      let ballLeftPosition = Math.floor((xPosition  -ballWidth)); //field.clientWidth
-      ball.style.marginTop = ballTopPosition+"px";
-      ball.style.marginLeft = ballLeftPosition+"px";
-      //alert("x = " + xPosition + " y = " + yPosition + "x = " + ballTopPosition + " y = " + ballLeftPosition)
+      let ballTopPosition = yPosition  - ballHeight;
+      let ballLeftPosition = xPosition  - ballWidth;
+      
+      if (yPosition<=bottomBorder&&xPosition<=rightBorder&&yPosition>ballHeight&&xPosition>ballWidth){
+        ball.style.marginTop = ballTopPosition+"px";
+        ball.style.marginLeft = ballLeftPosition+"px";
+      }
+      else if(xPosition>=rightBorder&&yPosition>=bottomBorder){
+        ball.style.marginTop = bottomBorder-ballHeight+"px";
+        ball.style.marginLeft = rightBorder-ballWidth+"px";
+      }
+      else if(xPosition<=ballWidth&&yPosition>bottomBorder){
+        ball.style.marginTop = bottomBorder-ballHeight+"px";
+        ball.style.marginLeft = 0+"px";
+      }
+      else if(yPosition<=ballHeight&&xPosition>rightBorder){
+        ball.style.marginTop = 0+"px";
+        ball.style.marginLeft = rightBorder-ballWidth+"px";
+      }
+      else if(yPosition>bottomBorder){
+        ball.style.marginTop = bottomBorder-ballHeight+"px";
+        ball.style.marginLeft = ballLeftPosition+"px";
+      }
+      else if(xPosition>rightBorder){
+        ball.style.marginTop = ballTopPosition+"px";
+        ball.style.marginLeft = rightBorder-ballWidth+"px";
+      }
+      else if(xPosition<=ballWidth&&yPosition<=ballHeight){
+        ball.style.marginTop = 0+"px";
+        ball.style.marginLeft = 0+"px";
+      }
+      else if(yPosition<=ballHeight){
+        ball.style.marginTop = 0+"px";
+        ball.style.marginLeft = ballLeftPosition+"px";
+      }
+      else if(xPosition<=ballWidth){
+        ball.style.marginTop = ballTopPosition+"px";
+        ball.style.marginLeft = 0+"px";
+      }
+
+
     });
 
   }   
